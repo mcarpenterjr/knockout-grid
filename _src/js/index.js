55c79319cb970a283523a00ccd07c38b8f424f43
Mark N Carpenter Jr <mcarpenter@smtcorp.com>
2018-05-15 15:32:01 -0400

    FEATURE: Table Data Objects

--- _src/js/index.js
+++ _src/js/index.js
@@ -1,14 +1,8 @@
 function grid_data() {
   var self = this;
-  self.items = [];
-  $.getJSON('php/getSampleData.php', function(data, textStatus) {
-      // console.log(data);
-      for (var i = 0; i < data.length; i++) {
-        self.items.push(data[i]);
-      }
-  });
+  self.grids = [];
 }
-var grid = new grid_data();
+var ko_grid = new grid_data();
 
 function magic_grid(data) {
   var self = this;
@@ -18,31 +12,175 @@ function magic_grid(data) {
   self.load = function() {
     var self = this;
     
-    var grids = $('ko-table');
+    var koGrids = $('ko-table');
 
-    if (grids.length > 0) {
-      $(grids).each(function(idx, el) {
-        var table;
-        if ($(el).attr('theme') == 'boot-strap') {
-          var hover = $(el).attr('hover') ? 'table-hover' : null,
-          bordered = $(el).attr('borders') ? 'table-bordered' : null,
-          compact = $(el).attr('compact') ? 'table-condensed' : null,
-          striped = $(el).attr('striped') ? 'table-striped' : null,
-          id = $(el).attr('table-id') ? $(el).attr('table-id') : null;
+    if (koGrids.length > 0) {
+      $(koGrids).each(function(idx, el) {
 
-          if ($(el).attr('responsive')) {
-            table = `<div class="table-responsive"><table id="${id}" class="table ${hover} ${bordered} ${compact} ${striped}"></table></div>`;
-          }
-          else {
-            table = `<table id="${id}" class="${hover} ${bordered} ${compact} ${striped}"></table>`;
-          }
-        }
-        $(el).append(table);
+        // MIND BLOWN -
+        // http://www.purplesquirrels.com.au/2013/04/getting-all-element-attribute-values-with-jquery/
+        options = [];
+        $.each($(el).get(0).attributes, function(i, attr) {
+          options[attr.name] = attr.value;
+        });
+        // console.log(options);
+
+        ko_grid.grids.push(new Grid(options));
       });
     }
   };
+
+  function Grid(data) {
+    var self = this;
+
+    self.id = data.id || null;
+    self.source = data.source || null;
+    self.fixed = data['fixed-header'] || null;
+    self.hover = data.hover || null;
+    self.compact = data.compact || null;
+    self.striped = data.striped || null;
+    self.bordered = data.borders || null;
+    self.template = data.theme || null;
+
+    // Header Data For table/grid
+    self.header = ko.observableArray();
+    // Body Data for table/grid
+    self.rows = ko.observableArray();
+    /**8                     DATA MODIFIERS                     8**/
+    /*                                                            */
+    /* NOTE: Data modifiers are flags that indicate if an action  */
+    /* is occurring. The definitions are below.                   */
+    /*                                                            */
+    /* @sortTog - Indicates if sorting is active on the tables    */
+    /* data.                                                      */
+    /* @sortDef - Indicates if the default sort is active on the  */
+    /* data.                                                      */
+    /* @scarping - Indicates if data is being refreshed or        */
+    /* activley being retreived.                                  */
+    /*                                                            */
+    /*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
+    self.dataMods = {
+      sortTog: ko.observable(false),
+      sortDef: ko.observable(false),
+      scraping: ko.observable(false)
+    };
+    // Filter Data
+    self.filters = ko.observableArray();
+    self.rows_sort_order = ko.observableArray();
+    self.rows_sorted = ko.pureComputed(function() {
+      var a = self.rows().slice(),
+        hasSort = self.dataMods.sortTog(),
+        defaultSort = self.dataMods.sortDef(),
+        sp = parseInt(self.paginated.selectedPage()),
+        spp = parseInt(self.paginated.selectedPerPage()),
+        args = self.rows_sort_order(),
+        filts = self.filters();
+
+      if (hasSort) {
+        if (defaultSort) {
+          args = defaults.concat(self.market_data_sort_order());
+        }
+        a.sort(app.makeSort(args));
+      }
+
+      if (filts.length > 0) {
+        for (var i = 0; i < filts.length; i++) {
+          var filter = filts[i];
+          a = a.filter(app.makeFilter(filter));
+        }
+        console.log(a);
+      }
+
+      var start = parseInt((sp - 1) * spp),
+        end = parseInt(sp * spp);
+      a = a.slice(start, end);
+
+      return a;
+    }).extend({
+      deferred: true
+    });
+    // Pagination Model
+    self.paginated = {
+      perPage: ko.observableArray([5,10,20,50,100,250,500,1000]),
+      selectedPerPage: ko.observable(100),
+      selectedPage: ko.observable(1),
+      totalResults: ko.computed(function() {
+        var vis = 0;
+        for (var i = 0; i < self.rows().length; ++i) {
+          var row = self.rows()[i];
+          vis++;
+          // if (row.fm.visible()) {
+          //   // visibility conditional if needed.
+          // }
+        }
+        return vis;
+      }),
+      totalPagesList: ko.observableArray(),
+      totalPages: ko.pureComputed(function() {
+        var pp = self.paginated.selectedPerPage(),
+          tr = self.paginated.totalResults(),
+          tp = [];
+        var tpInt = Math.ceil(parseInt(tr) / parseInt(pp));
+
+        var i = 0;
+        do {
+          i += 1;
+          tp.push({page: i});
+        } while(i < tpInt);
+        // console.log(tp)
+        self.paginated.selectedPage(1);
+        // Callbacks for after pagination has completed.
+        // app.makeTableNice('#part-view');
+        // app.makeTableNice('.watch-table');
+        return tp;
+      }).extend({
+        deferred: true
+      })
+    };
+
+  }
+  Grid.prototype.getData = function() {
+    var self = this;
+    $.getJSON('php/getSampleData.php', function(data, textStatus) {
+      // console.log(data);
+      for (var i = 0; i < data.rows.length; i++) {
+        var row = data.rows[i];
+        self.rows.push(row);
+      }
+      for (var i = 0; i < data.header.length; i++) {
+        var colHeader = data.header[i];
+        // console.log(colHeader);
+        self.header.push(new ColHeader(colHeader));
+      }
+    });
+  };
+
+  function ColHeader(data) {
+    var self = this;
+
+    self.title = ko.observable(data.title);
+    self.col_name = ko.observable(data.col_name);
+    self.type = ko.observable(data.data_type);
+    self.sort = ko.observable(data.sort);
+    self.search = ko.observable(data.search);
+
+  }
+
+  function Row(data) {
+    var self = this;
+
+    // Object where all our computables are stored.
+    self.fm = {
+      visible: ko.computed(function() {
+        // Always visbile
+        return true;
+      })
+    };
+
+  }
+
 }
-var grid_model = new magic_grid(grid);
+var grid_model = new magic_grid(ko_grid);
 // init();
 
 $(document).ready(function() {
--- _src/php/getSampleData.php
+++ _src/php/getSampleData.php
@@ -4,8 +4,48 @@ require  "$root/inc/dbConn.php";
 
 $db = new dbConn('devbox');
 
-$SQL = "SELECT * FROM cetec.bin_scan ORDER BY last_scan DESC LIMIT 1000";
+$SQL = "SELECT
+          bin,
+          userid,
+          last_scan
+        FROM
+          cetec.bin_scan
+        ORDER BY
+          last_scan
+        DESC LIMIT
+          1000";
 
 $data = $db->getResult($SQL);
+$output['rows'] = $data;
 
-echo json_encode($data);
+// For this simplified case we do this setup manually without class.
+// Looking forward, if there was an API, this would be done once and done.
+// If we were to work at implementing a column class that looked at the 
+// select statement of our query we could automate some of the values here.
+// THIS IS A SUPER SIMPLIFICATION OF EXPECTED DATA.
+
+$output['header'] = array();
+
+$bin['title'] = "Bin";
+$bin['data_type'] = "mixed";
+$bin['sort'] = true;
+$bin['search'] = true;
+$bin['col_name'] = "bin";
+array_push($output['header'], $bin);
+
+$user['title'] = "User";
+$user['data_type'] = "string";
+$user['sort'] = true;
+$user['search'] = true;
+$user['col_name'] = "userid";
+array_push($output['header'], $user);
+
+$scan['title'] = "Last Scanned";
+$scan['data_type'] = "date";
+$scan['sort'] = true;
+$scan['search'] = true;
+$scan['col_name'] = "last_scan";
+array_push($output['header'], $scan);
+
+// We expect JSON
+echo json_encode($output);
